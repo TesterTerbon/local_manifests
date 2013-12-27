6430b927ac5e398f6e6d16c74bbbfa7bffc76223
TesterTerbon <tester.android.xda59@gmail.com>
2013-12-27 16:04:46 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 if [ -f ~/bin/paths-11.0.sh ]; then
         source ~/bin/paths-11.0.sh
 fi
@@ -7,6 +6,5 @@ fi
 if [ "${android}" = "" ]; then
         android=~/android/system
 fi
-cherries+=(48673)
 
 ${android}/build/tools/repopick.py -b ${cherries[@]}
